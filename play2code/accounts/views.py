8d1109518ba873e287c286543fe5dcdc3ef494b1
Manoj Singh <151902566+manojsingh7838@users.noreply.github.com>
2024-09-18 00:22:34 +0530

    Update views.py

--- play2code/accounts/views.py
+++ play2code/accounts/views.py
@@ -88,7 +88,7 @@ def otp_verification_view(request):
 
                     logger.info("OTP verified successfully.")
                     print("OTP verified successfully.")
-                    return redirect("/login")
+                    return redirect("account/login")
                 except Exception as e:
                     logger.error(f"Error while verifying OTP or activating user: {e}")
                     return render(
