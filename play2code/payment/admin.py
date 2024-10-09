d0b469ad34e3d52afb7cfc294f55396f9dc4b40f
Manoj Singh <151902566+manojsingh7838@users.noreply.github.com>
2024-10-09 15:32:27 +0530

    Update admin.py

--- play2code/payment/admin.py
+++ play2code/payment/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Payment)
+admin.site.register(CoursePayment)
+admin.site.register( MachineLearningPayment)
+admin.site.register(FullStackPayment)
+admin.site.register(FrontEndPayment)
+admin.site.register(BackEndPayment)
+admin.site.register(CCPayment)
+admin.site.register(DataAnalyticsPayment)
+admin.site.register(DataSciencePayment)
