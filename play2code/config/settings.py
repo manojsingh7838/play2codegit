66292d32b05a0659bf1bb9345dfe545ebfac721d
Manoj Singh <151902566+manojsingh7838@users.noreply.github.com>
2024-10-15 13:18:33 +0530

    Update settings.py

--- play2code/config/settings.py
+++ play2code/config/settings.py
@@ -157,13 +157,20 @@ CORS_ALLOWED_ORIGINS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "smtp.zoho.com"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = "play2code@play2code.xyz"
+# EMAIL_HOST_PASSWORD = "YUKbQN7U0cvS"
+# DEFAULT_FROM_EMAIL = "play2code@play2code.xyz"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.zoho.com"
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "play2code@play2code.xyz"
-EMAIL_HOST_PASSWORD = "YUKbQN7U0cvS"
-DEFAULT_FROM_EMAIL = "play2code@play2code.xyz"
+EMAIL_USE_TLS = True  
+EMAIL_HOST_USER = "support@seawealth.io"  
+EMAIL_HOST_PASSWORD = "gi2vazS%" 
+DEFAULT_FROM_EMAIL = "support@seawealth.io"  
 
 
 class MyCustomMiddleware:
