475c042588af04b2acf1a7d986fad850270a4e89
Manoj Singh <151902566+manojsingh7838@users.noreply.github.com>
2024-10-16 13:00:37 +0530

    Update settings.py

--- play2code/config/settings.py
+++ play2code/config/settings.py
@@ -183,10 +183,10 @@ DEFAULT_FROM_EMAIL = "play2code@play2code.xyz"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'play2codecontact@gmail.com'  # Your Gmail address
-EMAIL_HOST_PASSWORD = '@Manojsingh78'  # The app password you generated
+EMAIL_USE_TLS = True  
+EMAIL_USE_SSL = False 
+EMAIL_HOST_USER = 'play2codecontact@gmail.com'  
+EMAIL_HOST_PASSWORD = '@Manojsingh78'  
 
 
 
